cae5be3acf35d57e25f6b2e29f2c3c994767add0
yangjunhua <353196481@qq.com>
2016-05-26 13:04:25 +0800

    第一次
    重新搭建了场景，实现了人物的移动

---
+++ 2D_Example/Assets/Scripts/Playcontrol.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playcontrol : MonoBehaviour {
+
+    [HideInInspector]
+    public bool heroFaceRight = true;
+    [HideInInspector]
+    public bool heroJump = false;
+
+    public float MoveForce = 100f;
+    public float JumpForce = 600f;
+    public float MaxSpeed = 5f;
+    private bool IsGrounded = false;
+    private Transform mGroudCheck;
+
+    private Rigidbody2D Herobody;
+    private Animator anim;
+
+
+	void Awake()
+    {
+        mGroudCheck = transform.Find("GroundCheck");
+        Herobody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        IsGrounded = Physics2D.Linecast(transform.position, mGroudCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            heroJump = true;
+        }
+        Debug.DrawLine(transform.position, mGroudCheck.position, Color.red, 0.5f);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        float fInput = Input.GetAxis("Horizontal");
+        if((fInput*Herobody.velocity.x)<MaxSpeed)
+        {
+            Herobody.AddForce(Vector2.right * fInput * MoveForce);
+        }
+        if((fInput*Herobody.velocity.x)>MaxSpeed)
+        {
+            Herobody.velocity = new Vector2(Mathf.Sign(Herobody.velocity.x) * MaxSpeed, Herobody.velocity.y);
+        }
+        if(fInput>0&& !heroFaceRight)
+        {
+            flip();
+        }
+        else if(fInput<0&& heroFaceRight)
+        {
+            flip();
+        }
+        if(heroJump)
+        {
+          //  int i = Random.Range(0, JumpClips.Length);
+          //  AudioSource.PlayClipAtPoint(JumpClips[i], transform.position);
+            Herobody.AddForce(new Vector2(0f, JumpForce));
+            heroJump = false;
+        }
+	}
+
+    void flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        heroFaceRight = !heroFaceRight;
+    }
+}
